c82fcf82588a0578c7d717f49dceac5c9eebbe61
Sorion <maxime.pelte@gmail.com>
2018-03-05 16:16:08 +0100

    Setup Unity Project

---
+++ Assets/MoveSphereScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveSphereScript : MonoBehaviour {
+
+    [SerializeField]
+    private Transform[] sphereTransform;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("GlaDOS Start");
+        sphereTransform[0].position += Vector3.down * 2f;
+        sphereTransform[1].position += Vector3.up * 2f;
+        sphereTransform[2].position += Vector3.forward * 2f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
