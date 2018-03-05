55027e68d9fb428ba32f22fd0a9ba77ce6904e42
teddy.reino9@gmail.com <teddy.reino9@gmail.com>
2018-03-05 18:55:32 +0100

    Refacto en fonction

--- Assets/MoveSphereScript.cs
+++ Assets/MoveSphereScript.cs
@@ -11,11 +11,7 @@ public class MoveSphereScript : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		Debug.Log("GlaDOS Init");
-		for(int z = 0; z < 10; z++){
-			for(int x = 0; x < 10; x++){
-				Instantiate (preFabSphere, new Vector3 (x, 0, z), Quaternion.identity);
-			}
-		}
+		initSphere ();
 
 		Instantiate (preFabSphere, new Vector3 (3, -2, 4), Quaternion.identity);
 		Instantiate (preFabSphere, new Vector3 (1, 2, 2), Quaternion.identity);
@@ -27,6 +23,14 @@ public class MoveSphereScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	private void initSphere() {
+		for(int z = 0; z < 10; z++){
+			for(int x = 0; x < 10; x++){
+				Instantiate (preFabSphere, new Vector3 (x, 0, z), Quaternion.identity);
+			}
+		}
 	}
 
     [DllImport("GlaDOS")]
